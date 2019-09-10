4108bbee0ac894b6d67765be126187cae8bf5539
Nicholas Tsim <nicholastsim@gmail.com>
2019-09-10 21:42:03 +0100

    Update route-53-cluster-hostname to 0.3.0

--- main.tf
+++ main.tf
@@ -1589,10 +1589,8 @@ resource "aws_s3_bucket" "elb_logs" {
 }
 
 module "tld" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.5"
-  namespace = var.namespace
+  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.3.0"
   name      = var.name
-  stage     = var.stage
   zone_id   = var.zone_id
   records   = [aws_elastic_beanstalk_environment.default.cname]
   enabled   = length(var.zone_id) > 0 ? "true" : "false"
